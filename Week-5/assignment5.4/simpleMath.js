36fc0b04ceea1f051f4248753331a051adde03cb
shoreya gupta <gupta.shoreya2707@gmail.com>
2022-08-23 23:09:06 +0530

    JEST implementation for 3 functions

---
+++ Week-5/assignment5.4/simpleMath.js
@@ -0,0 +1,15 @@
+function sum(num1, num2){
+    return num1+num2
+}
+
+function diff(num1, num2){
+    return num1-num2
+}
+
+function product(num1, num2){
+    return num1*num2
+}
+
+module.exports.sum = sum
+module.exports.diff = diff
+module.exports.product = product
