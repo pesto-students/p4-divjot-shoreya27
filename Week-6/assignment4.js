9668cf86184fd895657d4ba35da6a1a4bc048d2a
shoreya gupta <gupta.shoreya2707@gmail.com>
2022-08-31 20:35:53 +0530

    added time complexity and problem statement in the code

--- Week-6/assignment4.js
+++ Week-6/assignment4.js
@@ -1,4 +1,8 @@
 // Best time to buy and sell stock
+// Problem 6.4 : Best time to buy and sell stockYou are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock andchoosing a different day in the future to sell that stock.
+// Return the maximum profit youcan achieve from this transaction. 
+// If you cannot achieve any profit, return 0.
 function bestTimeCalc(arr){
     let min=arr[0];
     let min_index = 0
@@ -25,3 +29,6 @@ function bestTimeCalc(arr){
 }
 
 bestTimeCalc([10,7,6,19,1,19,2,18,2,5,30])
+
+//TimeComplexity: O(N)
+//Space Complexity:O(1)
